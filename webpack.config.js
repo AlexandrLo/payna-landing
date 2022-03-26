1979b8c8fce88d089731aa337143280cc06b7672
AlexandrLo <alexandr.lo@outlook.com>
2022-03-26 22:23:05 +0300

    Add image assets rule

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.(png|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
 				test: /\.svg$/,
 				use: ["@svgr/webpack"],
 			},
