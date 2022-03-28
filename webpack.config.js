17919ac42aacad235a88c240e856339f6f898383
AlexandrLo <alexandr.lo@outlook.com>
2022-03-28 23:35:26 +0300

    Move index.html

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.join(__dirname, "public", "index.html"),
+			template: path.join(__dirname, "src", "index.html"),
 		}),
 	],
 	module: {
