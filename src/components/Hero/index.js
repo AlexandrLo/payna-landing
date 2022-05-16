948b7e545d73152aa6391b13f9768b14e7ac0f8b
Alexandr <alexandr.lo@outlook.com>
2022-05-16 21:13:08 +0300

    Update Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,49 +1,45 @@
 import React from "react";
 import {
-	Box,
-	Container,
-	Text,
-	HStack,
-	VStack,
-	Button,
-	Heading,
-	Show,
-	Image,
+  Button,
+  Container,
+  HStack,
+  Heading,
+  Image,
+  Show,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
-import HeroImage from "assets/hero-image.png";
 
 function Hero() {
-	return (
-		<Container
-			size="md"
-			pt={{ base: "0.5rem", md: "3.75rem" }}
-			paddingRight={{ xl: "0.25rem" }}
-		>
-			<HStack
-				justifyContent={{ base: "start", md: "center", xl: "space-between" }}
-			>
-				<VStack
-					maxW={{ md: "28.125rem", xl: "100%" }}
-					alignItems={{ base: "start", md: "center", xl: "start" }}
-					textAlign={{ base: "start", md: "center", xl: "start" }}
-					spacing={{ base: "1.6rem", md: "1.65rem" }}
-				>
-					<Heading as="h1" size="3xl">
-						Manage Payroll Like an Expert
-					</Heading>
-					<Text color="muted" lineHeight="taller">
-						Payna is helping you to setting up the payroll without required any
-						finance skills or knowledge before
-					</Text>
-					<Button colorScheme="blue">Get Started</Button>
-				</VStack>
+  return (
+    <Container
+      size="md"
+      pt={{ base: "0.5rem", md: "3.75rem" }}
+      pr={{ xl: "0.25rem" }}
+    >
+      <HStack justify={{ base: "start", md: "center", xl: "space-between" }}>
+        <VStack
+          maxW={{ md: "28.125rem", xl: "100%" }}
+          align={{ base: "start", md: "center", xl: "start" }}
+          textAlign={{ base: "start", md: "center", xl: "start" }}
+          spacing={{ base: "1.6rem", md: "1.65rem" }}
+        >
+          <Heading as="h1" size="3xl">
+            Manage Payroll Like an Expert
+          </Heading>
+          <Text color="muted" lineHeight="taller">
+            Payna is helping you to setting up the payroll without required any
+            finance skills or knowledge before
+          </Text>
+          <Button colorScheme="blue">Get Started</Button>
+        </VStack>
 
-				<Show above="xl">
-					<Image src={HeroImage} alt="Hero image" />
-				</Show>
-			</HStack>
-		</Container>
-	);
+        <Show above="xl">
+          <Image src="assets/images/hero-image.png" alt="Hero image" />
+        </Show>
+      </HStack>
+    </Container>
+  );
 }
 
 export default Hero;
