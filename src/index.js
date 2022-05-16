9215edceaa9a673c357f8f7d5529e4d449863a16
Alexandr <alexandr.lo@outlook.com>
2022-05-16 20:51:47 +0300

    Add customLocalStorageManager

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "theme";
 
 import App from "./App";
+import { customLocalStorageManager } from "utils/customLocalStorageManager";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ChakraProvider theme={theme}>
 			<BrowserRouter basename="/payna-landing">
 				<App />
 			</BrowserRouter>
 		</ChakraProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
+    <ChakraProvider theme={theme} colorModeManager={customLocalStorageManager}>
 );
