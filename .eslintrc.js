889227de41fb2940366309aacf0722e5d4521555
Alexandr <alexandr.lo@outlook.com>
2022-05-16 20:49:58 +0300

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "sort-imports": [
+      "error",
+      {
+        memberSyntaxSortOrder: ["none", "all", "single", "multiple"],
+        allowSeparatedGroups: true,
+      },
+    ],
+  },
+};
