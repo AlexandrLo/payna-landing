9dd100522ff2bcce6e4cd220a75b95d59b68cc7f
Alexandr <alexandr.lo@outlook.com>
2022-05-16 21:28:14 +0300

    Update Features

--- src/components/Features/Feature.js
+++ src/components/Features/Feature.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { Text, HStack, VStack, Heading, Circle } from "@chakra-ui/react";
 
-function Feature(props) {
-	return (
-		<HStack align="start" spacing="1.6rem" maxW="320px">
-			<Circle size="3.75rem" bg={props.iconBg} color="white">
-				{props.icon({ size: "1.5rem" })}
-			</Circle>
-			<VStack align="start" spacing="0.75rem">
-				<Heading as="h3" size="md" pt="0rem">
-					{props.heading}
-				</Heading>
-				<Text color="muted" lineHeight="2rem">
-					{props.text}
-				</Text>
-			</VStack>
-		</HStack>
-	);
+import PropTypes from "prop-types";
+import { Circle, HStack, Heading, Text, VStack } from "@chakra-ui/react";
+
+function Feature({ icon, iconBg, heading, text }) {
+  return (
+    <HStack align="start" spacing="1.6rem" maxW="320px">
+      <Circle size="3.75rem" bg={iconBg} color="white">
+        {icon({ size: "1.5rem" })}
+      </Circle>
+      <VStack align="start" spacing="0.75rem">
+        <Heading as="h3" size="md" pt="0rem">
+          {heading}
+        </Heading>
+        <Text color="muted" lineHeight="2rem">
+          {text}
+        </Text>
+      </VStack>
+    </HStack>
+  );
 }
+
+Feature.propTypes = {
+  icon: PropTypes.func.isRequired,
+  iconBg: PropTypes.string.isRequired,
+  heading: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Feature;
--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -1,82 +1,95 @@
 import React from "react";
-import { Container, Text, VStack, Heading, SimpleGrid } from "@chakra-ui/react";
+
+import { Container, Heading, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import {
-	FiBriefcase,
-	FiSend,
-	FiWifiOff,
-	FiTrello,
-	FiGift,
-	FiGlobe,
+  FiBriefcase,
+  FiGift,
+  FiGlobe,
+  FiSend,
+  FiTrello,
+  FiWifiOff,
 } from "react-icons/fi";
+
 import Feature from "./Feature";
 
-function Features() {
-	return (
-		<Container size="md" pt={{ base: "5rem", md: "8.125rem" }}>
-			<VStack spacing={{ base: "3rem", md: "4.25rem" }}>
-				{/* Heading */}
-				<VStack spacing="1.25rem">
-					<VStack spacing="0.5rem">
-						<Text fontWeight="700" color="red.400" textAlign="center">
-							WORK BETTER
-						</Text>
-						<Heading as="h2" size="lg" textAlign="center">
-							For Your Business
-						</Heading>
-					</VStack>
+const featuresData = [
+  {
+    icon: FiBriefcase,
+    iconBg: "#1F7CFF",
+    heading: "Share Insights",
+    text: "Working together with your team to make decisions",
+  },
+  {
+    icon: FiSend,
+    iconBg: "#F75C4E",
+    heading: "Track Leads",
+    text: "See where your money goes and comes in business",
+  },
+  {
+    icon: FiWifiOff,
+    iconBg: "#191046",
+    heading: "Offline Mode",
+    text: "Use the feature while off from internet? sure can",
+  },
+  {
+    icon: FiTrello,
+    iconBg: "#FF1FB3",
+    heading: "Kanban Mode",
+    text: "Organize the report that easy to be understand",
+  },
+  {
+    icon: FiGift,
+    iconBg: "#5C4EF7",
+    heading: "Reward System",
+    text: "Motivate your team working harder and receive a gift",
+  },
+  {
+    icon: FiGlobe,
+    iconBg: "#F7954E",
+    heading: "189 Country",
+    text: "Working together worldwide people from anywhere",
+  },
+];
 
-					<Text color="muted" maxW="25rem" textAlign="center" lineHeight="2rem">
-						We did research what your company needs and here we are providing
-						all of them just for you
-					</Text>
-				</VStack>
-				{/* Grid */}
-				<SimpleGrid
-					columns={{ base: 1, md: 2, lg: 3 }}
-					spacingY="3.75rem"
-					spacingX="5.25rem"
-					maxW="1105px"
-				>
-					<Feature
-						icon={FiBriefcase}
-						iconBg="#1F7CFF"
-						heading="Share Insights"
-						text="Working together with your team to make decisions"
-					/>
-					<Feature
-						icon={FiSend}
-						iconBg="#F75C4E"
-						heading="Track Leads"
-						text="See where your money goes and comes in business"
-					/>
-					<Feature
-						icon={FiWifiOff}
-						iconBg="#191046"
-						heading="Offline Mode"
-						text="Use the feature while off from internet? sure can"
-					/>
-					<Feature
-						icon={FiTrello}
-						iconBg="#FF1FB3"
-						heading="Kanban Mode"
-						text="Organize the report that easy to be understand"
-					/>
-					<Feature
-						icon={FiGift}
-						iconBg="#5C4EF7"
-						heading="Reward System"
-						text="Motivate your team working harder and receive a gift"
-					/>
-					<Feature
-						icon={FiGlobe}
-						iconBg="#F7954E"
-						heading="189 Country"
-						text="Working together worldwide people from anywhere"
-					/>
-				</SimpleGrid>
-			</VStack>
-		</Container>
-	);
+function Features() {
+  return (
+    <Container size="md" pt={{ base: "5rem", md: "8.125rem" }}>
+      <VStack spacing={{ base: "3rem", md: "4.25rem" }}>
+        {/* Heading */}
+        <VStack spacing="1.25rem">
+          <VStack spacing="0.5rem">
+            <Text fontWeight="700" color="red.400" textAlign="center">
+              WORK BETTER
+            </Text>
+            <Heading as="h2" size="lg" textAlign="center">
+              For Your Business
+            </Heading>
+          </VStack>
+          <Text color="muted" maxW="25rem" textAlign="center" lineHeight="2rem">
+            We did research what your company needs and here we are providing
+            all of them just for you
+          </Text>
+        </VStack>
+        {/* Grid */}
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacingY="3.75rem"
+          spacingX="5.25rem"
+          maxW="1105px"
+        >
+          {featuresData.map((featureData, index) => (
+            <Feature
+              key={index}
+              icon={featureData.icon}
+              iconBg={featureData.iconBg}
+              heading={featureData.heading}
+              text={featureData.text}
+            />
+          ))}
+        </SimpleGrid>
+      </VStack>
+    </Container>
+  );
 }
 
 export default Features;
