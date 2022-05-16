b67294fe90e69b6902d36ca83b75395f80ed9ab4
Alexandr <alexandr.lo@outlook.com>
2022-05-16 21:11:56 +0300

    Update NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,45 +1,53 @@
 import React from "react";
-import { Box, Container, Link, HStack, Button, Show } from "@chakra-ui/react";
-import { Link as RouterLink } from "react-router-dom";
-import Logo from "assets/logo.svg";
+
+import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Link,
+  Show,
+} from "@chakra-ui/react";
+
+import Logo from "assets/svg/logo.svg";
 
 function NavBar() {
-	return (
-		<Box paddingY="2.5rem">
-			<Container size="lg">
-				<HStack>
-					<Box w="100%" position="relative">
-						<Box translateY="-0.2rem" transform="auto">
-							<Logo />
-						</Box>
-					</Box>
-					<Show above="lg">
-						<HStack w="auto" spacing="3.75rem">
-							<Link as={RouterLink} to="/" variant="active">
-								Home
-							</Link>
-							<Link as={RouterLink} to="/">
-								Features
-							</Link>
-							<Link as={RouterLink} to="/">
-								Showcase
-							</Link>
-							<Link as={RouterLink} to="/">
-								Pricing
-							</Link>
-						</HStack>
-					</Show>
-					<Show above="sm">
-						<Box w="100%" display="flex" justifyContent="end">
-							<Button colorScheme="gray" fontWeight="400">
-								Sign In
-							</Button>
-						</Box>
-					</Show>
-				</HStack>
-			</Container>
-		</Box>
-	);
+  return (
+    <Container size="lg" py="2.5rem">
+      <HStack>
+        <Box w="100%" position="relative">
+          <Box translateY="-0.2rem" transform="auto">
+            <Logo />
+          </Box>
+        </Box>
+        <Show above="lg">
+          <HStack w="auto" spacing="3.75rem">
+            <Link as={NavLink} to="/">
+              Home
+            </Link>
+            <Link as={NavLink} to="/features">
+              Features
+            </Link>
+            <Link as={NavLink} to="/showcase">
+              Showcase
+            </Link>
+            <Link as={NavLink} to="/pricing">
+              Pricing
+            </Link>
+          </HStack>
+        </Show>
+        <Show above="sm">
+          <Flex w="100%" display="flex" justify="end">
+            <Button colorScheme="gray" fontWeight="400">
+              Sign In
+            </Button>
+          </Flex>
+        </Show>
+      </HStack>
+    </Container>
+  );
 }
 
 export default NavBar;
