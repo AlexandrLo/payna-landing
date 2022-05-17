6dda94ca68c37e67c0728acdaf357484d4a70665
Alexandr <alexandr.lo@outlook.com>
2022-05-17 18:50:54 +0300

    Add NotFoundPage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from "containers/HomePage";
 import NavBar from "./components/NavBar";
+import NotFoundPage from "containers/NotFoundPage";
 
 export default function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </>
---
+++ src/containers/NotFoundPage/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { Button, Container, Heading, VStack } from "@chakra-ui/react";
+
+function NotFoundPage() {
+  return (
+    <Container
+      size="md"
+      pt={{ base: "0.5rem", md: "3.75rem" }}
+      pr={{ xl: "0.25rem" }}
+    >
+      <VStack spacing="2rem" py="4rem">
+        <Heading as="h1" size="3xl">
+          Page Not Found...
+        </Heading>
+        <Button as={Link} to="/" colorScheme="blue">
+          Back to Homepage
+        </Button>
+      </VStack>
+    </Container>
+  );
+}
+
+export default NotFoundPage;
